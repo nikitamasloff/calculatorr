be96dbb16501a5ce16359b4cab5fe24269b205a6
Nikita Maslov <developer.nikita.maslov@gmail.com>
2019-10-27 10:40:44 +0300

    Change namings

--- app/src/main/java/com/exmpl/calculatorr/data/calc/Calculatorr.java
+++ app/src/main/java/com/exmpl/calculatorr/data/calc/Calculatorr.java
@@ -5,7 +5,7 @@ import com.exmpl.calculatorr.data.model.expr.Expression;
 import com.exmpl.calculatorr.data.model.expr.Notated;
 import com.exmpl.calculatorr.data.model.oprnd.Operand;
 
-public interface Calculator<T extends Expression & Notated, R extends Operand> {
+public interface Calculatorr<T extends Expression & Notated, R extends Operand> {
 
     R calculate(T expr) throws NaNException;
 }
